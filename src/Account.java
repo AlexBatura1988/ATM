fee0b01227c0f232409aadfd4bdae81213ba9398
alexb <alex.batura88@gmail.com>
2022-03-09 00:02:41 +0200

    User

--- src/Account.java
+++ src/Account.java
@@ -1,2 +1,9 @@
+import java.util.ArrayList;
+
 public class Account {
+    private String name;
+    private double balance;
+    private String uuid;
+    private User holder;
+    private ArrayList<Transaction> transactions;
 }
